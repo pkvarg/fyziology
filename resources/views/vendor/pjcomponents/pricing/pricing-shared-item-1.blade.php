0ef957fd1e7423a9efe67b2c7915c60de5b69388
peter varga <peter.varga@staffino.com>
2025-10-05 11:21:36 +0300

    card one ok with circle

--- resources/views/vendor/pjcomponents/pricing/pricing-shared-item-1.blade.php
+++ resources/views/vendor/pjcomponents/pricing/pricing-shared-item-1.blade.php
@@ -1,6 +1,8 @@
 <div {{ $attributes->merge(['class' => 'pricing-18-item flex h-full flex-col justify-start bg-white rounded-3xl px-8 py-8 md:px-10 md:py-10 shadow-sm relative overflow-hidden']) }}>
     <!-- Blue gradient corner decoration -->
-    <div class="absolute top-0 right-0 w-32 h-32 bg-gradient-to-bl from-blue-200 to-transparent rounded-bl-full opacity-40 pointer-events-none"></div>
+    <div class="absolute -top-[25rem] -right-[25rem] w-[50rem] h-[50rem] pointer-events-none opacity-50">
+        <img src="{{ asset('assets/svg/Gradient circle.svg') }}" alt="" class="w-full h-full object-contain">
+    </div>
 
     <div class="relative z-10">
         <div class="mb-6 md:mb-6">
