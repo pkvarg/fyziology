24ffaa48f533d18e0c565e8bf0a58691764fb98b
peter varga <peter.varga@staffino.com>
2025-10-08 15:53:21 +0300

    pricing ok

--- resources/views/pages/pricing.blade.php
+++ resources/views/pages/pricing.blade.php
@@ -123,35 +123,32 @@
                     </x-pjcomponents::pricing.pricing-shared-item-1>
                 </x-slot:prices>
             </x-pjcomponents::pricing.pricing-18c1>
+            
         </div>
 
         <!-- Gradient Circle -  bottom right near CTA -->
          <div class="absolute md:hidden lg:inline top-[84rem] lg:top-[42rem] -right-48 lg:-right-96 w-[100%] lg:w-[50%] h-auto pointer-events-none opacity-60">
         <img src="{{ asset('assets/svg/Gradient circle.svg') }}" alt="" class="w-full h-full object-contain" />
-    </div>
-
-        <section class="px-[5%] py-16 md:py-24 lg:py-16 relative">
-    
-
-            <div class="container mx-auto w-full max-w-4xl relative z-10">
-     
-                       <!-- Gradient Half Circle - bottom right -->
-        <div class="lg:flex absolute top-[0rem] -left-[3rem] lg:top-0 lg:-left-[8rem] w-40 h-40  pointer-events-none">
-            <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-28 lg:w-full h-28 lg:h-full object-contain" />
         </div>
 
-                <h2 class="text-4xl md:text-5xl lg:text-4xl font-black mb-6 md:mb-8 ml-12 lg:ml-0">Získajte prvú konzultáciu zdarma</h2>
-                <p class="text-lg md:text-xl mb-8 md:mb-10 max-w-3xl text-gray-700">
+        
+
+         <div class="container w-full max-w-3xl lg:max-w-4xl relative z-10 py-16  lg:mx-auto">
+                <!-- Gradient Half Circle CTA - left -->
+                <div class="lg:flex absolute top-[4.3rem] -left-[2.75rem] md:-left-[4.25rem] lg:top-[4.2rem] lg:-left-[6.5rem] w-40 h-40 pointer-events-none px-[5%]">
+                    <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-20 h-20 lg:h-20 object-contain" />
+                </div>
+
+                <h2 class="text-[22px] md:text-xl font-black mb-6 md:mb-8 ml-10 lg:ml-0">Získajte prvú konzultáciu zdarma</h2>
+                <p class="text-sm md:text-md mb-8 md:mb-10 max-w-3xl text-gray-700 ml-10 lg:ml-0">
                     Sme tu pre Vás, pripravení sprevádzať Vás na ceste k lepšiemu zdraviu, väčšej pohyblivosti a celkovej pohode. Veríme, že každý krok smerom k uzdraveniu má zmysel – a tešíme sa, že môžeme byť jeho súčasťou.
                 </p>
-                <!-- <a href="{{ route('page.contact') }}" class="inline-block bg-primary-color text-white px-8 py-3 rounded-full text-lg font-semibold hover:opacity-90 transition-opacity">
-                    Vstupná konzultácia ZDARMA
-                </a> -->
+                <div class="flex lg:justify-start justify-center">
+                    <a href="{{ route('page.contact') }}" class="inline-block bg-primary-color text-white px-8 py-3 rounded-full text-lg font-semibold hover:opacity-90 transition-opacity ml-0 lg:-ml-16 mt-4">
+                        Vstupná konzultácia ZDARMA
+                    </a>
+                </div>
             </div>
-                 <a href="{{ route('page.contact') }}" class="inline-block bg-primary-color text-white px-8 py-3 rounded-full text-lg font-semibold hover:opacity-90 transition-opacity lg:ml-[10rem] mt-4">
-                    Vstupná konzultácia ZDARMA
-                </a>
-        </section>
 
         <x-footer />
     </div>
