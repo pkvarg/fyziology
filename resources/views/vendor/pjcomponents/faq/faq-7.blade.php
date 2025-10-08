c863598297aad7282cd326b0abf16f4650ce9ec7
peter varga <peter.varga@staffino.com>
2025-10-08 15:57:51 +0300

    ctc ok

--- resources/views/vendor/pjcomponents/faq/faq-7.blade.php
+++ resources/views/vendor/pjcomponents/faq/faq-7.blade.php
@@ -12,12 +12,12 @@
 
         <div class="mb-12 text-left md:mb-18 lg:mb-20">
              <!-- Gradient Half Circle - bottom right -->
-        <div class="lg:flex absolute top-[0rem] -left-[2rem] lg:top-0 lg:-left-[8rem] w-40 h-40  pointer-events-none">
-            <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-full h-20 lg:h-full object-contain" />
+        <div class="lg:flex absolute top-[0rem] -left-[2rem] lg:top-[0.5rem] lg:-left-[4rem] w-40 h-40  pointer-events-none">
+            <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-20 h-20 lg:h-20 object-contain" />
         </div>
-            <h2 class="mb-5 text-4xl font-black md:mb-6 md:text-6xl ml-8 lg:ml-0">{{ $heading }}</h2>
+            <h2 class="mb-5 text-2xl font-black md:mb-6 md:text-4xl ml-8 lg:ml-0">{{ $heading }}</h2>
             @isset($text)
-                <p class="md:text-md ml-0 lg:ml-0">{{ $text }}</p>
+                <p class="md:text-md ml-8 lg:ml-0">{{ $text }}</p>
             @endisset
         </div>
         <div class="grid grid-cols-1 gap-x-12 gap-y-10 md:gap-y-12">
