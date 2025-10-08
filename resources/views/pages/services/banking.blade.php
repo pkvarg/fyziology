cff4cb09208dc05ab254473897f24ce6a84b4c99
peter varga <peter.varga@staffino.com>
2025-10-08 12:22:47 +0300

    banking half circ mobile

--- resources/views/pages/services/banking.blade.php
+++ resources/views/pages/services/banking.blade.php
@@ -3,7 +3,7 @@
 <x-layout>
     <div class="banking bg-gray-50 mt-8">
         <!-- Hero Section with Title -->
-        <section class="px-[5%] py-16 md:py-20 lg:py-16 relative">
+        <section class="px-[5%] pt-16 pb-8 md:py-20 lg:pt-16 lg:pb-8 relative">
             <!-- Gradient Circle - top left -->
             <div class="absolute md:hidden lg:inline -top-24 lg:-top-[12rem] -left-48 lg:-left-96 w-[100%] lg:w-[50%] h-auto pointer-events-none opacity-60">
                 <img src="{{ asset('assets/svg/Gradient circle.svg') }}" alt="" class="w-full h-full object-contain" />
@@ -38,50 +38,50 @@
                 <div class="lg:flex absolute top-[0.2rem] -left-[2rem] -lg:top-[40rem] lg:-left-[1rem] w-40 h-40 pointer-events-none">
                     <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-full h-20 lg:h-full object-contain" />
                 </div>
-                    <h3 class="text-2xl md:text-3xl font-black mb-6 ml-8 lg:ml-0 ">Ako funguje bankovanie?</h3>
+                    <h3 class="text-2xl md:text-3xl font-black mb-6 ml-8 lg:ml-0">Ako funguje bankovanie?</h3>
 
-                    <p class="mt-64 lg:mt-0 ml-8 lg:ml-0">
+                    <p class="!mt-6 lg:mt-0 ml-8 lg:ml-0">
                         Pri bankovaní sa na pokožku umiestnia špeciálne sklenené alebo plastové "banky", ktoré sa nasajú na pokožku pomocou tepla alebo vákuovej pumpy. Tento proces vytvára podtlak, ktorý má niekoľko účinkov:
                     </p>
 
-                    <p>
+                    <p class="ml-8 lg:ml-0">
                         <span class="font-bold primary-color">Zvýšenie prietoku krvi:</span> Podtlak z banky zlepšuje cirkuláciu krvi v danom mieste, čo môže pomôcť pri regenerácii tkanív.
                     </p>
 
-                    <p>
+                    <p class="ml-8 lg:ml-0">
                         <span class="font-bold primary-color">Uvoľnenie svalov:</span> Pomáha zmierniť svalové napätie a bolestivosť tým, že podporí uvoľnenie svalových tkanív.
                     </p>
 
-                    <p>
+                     <p class="ml-8 lg:ml-0">
                         <span class="font-bold primary-color">Zmiernene bolesti:</span> Môže pomôcť pri liečbe bolesti v danom segmente.
                     </p>
 
-                    <p>
+                      <p class="ml-8 lg:ml-0">
                         <span class="font-bold primary-color">Zlepšenie pohyblivosti:</span> Uvoľnenie svalového napätia môže viesť k zlepšeniu rozsahu pohybu, čo je dôležité pri rehabilitácii po zraneniach alebo operáciách.
                     </p>
 
-                    <p>
+                    <p class="ml-8 lg:ml-0">
                         <span class="font-bold primary-color">Zlepšenie regenerácie tkanív</span>
                     </p>
 
-                    <p>
+                    <p class="ml-8 lg:ml-0">
                         <span class="font-bold primary-color">Pozitívne:</span> pôsobí na lymfatický, imunitný a nervový systém.
                     </p>
 
-                    <p>
+                    <p class="ml-8 lg:ml-0">
                         <span class="font-bold primary-color">Pozitívne:</span> ovplyvňuje vnútorné orgány (obličky ,žalúdok, slezinu, pečeň, žlčník, vnútorné orgány).
                     </p>
 
-                    <p>
+                    <p class="ml-8 lg:ml-0">
                         <span class="font-bold primary-color">Zlepšuje:</span> krvný obeh, čistí krv a pozitívne vplýva na činnosť srdca.
                     </p>
 
-                    <p>
+                     <p class="ml-8 lg:ml-0">
                         <span class="font-bold primary-color">Pomáha:</span> pri reumatizme či anémii.
                     </p>
 
-                    <h4 class="font-bold primary-color mt-8">Kontraindikácie:</h4>
-                    <ul class="list-none space-y-2">
+                    <h4 class="font-bold primary-color mt-8 ml-8 lg:ml-0">Kontraindikácie:</h4>
+                    <ul class="list-none space-y-2 ml-8 lg:ml-0">
                         <li>- tehotenstvo</li>
                         <li>- čerstvé jazvy po operáciách</li>
                         <li>- aneuryzmy</li>
@@ -114,7 +114,7 @@
         </section>
 
         <!-- Lightbox Modal -->
-        <div id="lightbox" class="fixed inset-0 bg-black bg-opacity-90 hidden items-center justify-center p-4" style="z-index: 9999;">
+        <!-- <div id="lightbox" class="fixed inset-0 bg-black bg-opacity-90 hidden items-center justify-center p-4" style="z-index: 9999;">
             <button id="close-btn" class="absolute top-4 right-4 md:top-8 md:right-8 text-white font-bold hover:text-gray-300 transition-colors w-16 h-16 md:w-20 md:h-20 flex items-center justify-center leading-none cursor-pointer bg-red-600 hover:bg-red-700 rounded-full" style="z-index: 10000; font-size: 3rem;">×</button>
             <img id="lightbox-img" src="" alt="Full size" class="max-w-full max-h-full object-contain rounded-lg">
         </div>
@@ -152,21 +152,18 @@
                     closeLightbox();
                 }
             });
-        </script>
+        </script> -->
 
         <!-- CTA Section -->
-        <div class="container mx-auto w-full max-w-6xl lg:max-w-4xl relative z-10 px-[5%] lg:px-0 py-16 overflow-hidden">
-    <!-- Gradient Circle - mobile only bottom right near Gallery -->
-    <div class="absolute md:hidden -top-[5rem]  -right-48  w-[100%] lg:w-[50%] h-auto pointer-events-none opacity-60">
-        <img src="{{ asset('assets/svg/Gradient circle.svg') }}" alt="" class="w-full h-full object-contain" />
-    </div>
+            <!-- CTA Section -->
+        <div class="container mx-auto w-full max-w-6xl lg:max-w-4xl relative z-10 px-[5%] lg:px-0 py-16">
             <!-- Gradient Half Circle CTA - left -->
-            <div class="lg:flex absolute top-[4rem] -left-[2rem] lg:top-[3.6rem] lg:-left-[8rem] w-40 h-40 pointer-events-none px-[5%]">
-                <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-full h-20 lg:h-full object-contain" />
+            <div class="lg:flex absolute top-[4.3rem] -left-[2rem] lg:top-[4.25rem] lg:-left-[6.5rem] w-40 h-40 pointer-events-none px-[5%]">
+                <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-20 h-20 lg:h-20 object-contain" />
             </div>
 
-            <h2 class="text-3xl md:text-3xl lg:text-3xl font-black mb-6 md:mb-8 ml-8 lg:ml-0">Získajte prvú konzultáciu zdarma</h2>
-            <p class="text-lg md:text-lg mb-8 md:mb-10 max-w-3xl text-gray-700">
+            <h2 class="text-[22px] md:text-xl font-black mb-6 md:mb-8 ml-8 lg:ml-0">Získajte prvú konzultáciu zdarma</h2>
+            <p class="text-md md:text-md mb-8 md:mb-10 max-w-3xl text-gray-700 ml-8 lg:ml-0">
                 Sme tu pre Vás, pripravení sprevádzať Vás na ceste k lepšiemu zdraviu, väčšej pohyblivosti a celkovej pohode. Veríme, že každý krok smerom k uzdraveniu má zmysel – a tešíme sa, že môžeme byť jeho súčasťou.
             </p>
             <div class="flex lg:justify-start justify-center">
@@ -176,8 +173,8 @@
         </div>
         </div>
 
-
+     
 
         <x-footer />
-    </div>
+   
 </x-layout>
