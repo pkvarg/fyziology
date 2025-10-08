1a160fd10292ac91f43781587fd38a1a4590c1b3
peter varga <peter.varga@staffino.com>
2025-10-08 10:59:37 +0300

    mob a amb half circles desktop

--- resources/views/pages/services/mobile-physiotherapy.blade.php
+++ resources/views/pages/services/mobile-physiotherapy.blade.php
@@ -24,7 +24,7 @@
                 <h2 class="text-3xl md:text-4xl lg:text-3xl font-black mb-8 text-center ">Mobilná fyzioterapia</h2>
 
                 <!-- Hero Image -->
-                <div class="mb-12 mt-16">
+                <div class="mb-8 mt-16 lg:mt-8">
                     <img src="{{ asset('assets/images/mobilna-fyzioterapia.jpg') }}" alt="Mobilná fyzioterapia" class="w-full max-w-[65rem] mx-auto rounded-3xl shadow-lg object-cover" style="max-height: 400px;">
                 </div>
                 
@@ -41,20 +41,12 @@
 
                 <!-- Výhody mobilnej fyzioterapie -->
                 <div class="text-left space-y-4 mb-8 lg:px-[10%] relative">
- <!-- Gradient Half Circle - left -->
-                    <!-- <div class="lg:flex absolute top-[0.2rem] -left-[2rem] lg:top-0 lg:-left-[1rem] w-40 h-40 pointer-events-none">
-                        <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-full h-20 lg:h-full object-contain" />
-                    </div> -->
 
-                
-
-
-
-                         <!-- Gradient Half Circle - left -->
-                <div class="lg:flex absolute top-[0.2rem] -left-[2rem] -lg:top-[40rem] lg:-left-[1rem] w-40 h-40 pointer-events-none">
-                    <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-full h-20 lg:h-full object-contain" />
+  <!-- Gradient Half Circle - left -->
+                <div class="lg:flex absolute top-[0.2rem] -left-[2rem] -lg:top-[40rem] lg:left-[3.25rem] w-40 h-40 pointer-events-none">
+                    <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-20 h-20 lg:h-20 object-contain" />
                 </div>
-                    <h3 class="text-2xl md:text-3xl font-black mb-6 ml-8 lg:ml-0 ">Výhody mobilnej fyzioterapie</h3>
+                    <h3 class="text-2xl md:text-xl font-black mb-6 ml-8 lg:ml-0 ">Výhody mobilnej fyzioterapie</h3>
 
                     <p class="!mt-[1.5rem] lg:mt-0 ml-[2.25rem] lg:ml-0">
                         <span class="font-bold primary-color">Pohodlie a flexibilita:</span> Výhodou je, že pacienti nemusia cestovať mimo bydliska a hlavne pre tých, ktorí majú problémy s mobilitou, alebo pre starších ľudí a iných s chronickými zdravotnými problémami.
@@ -97,7 +89,7 @@
                 <h2 class="text-3xl md:text-4xl lg:text-3xl font-black mb-8 text-center ">Ambulantná fyzioterapia</h2>
 
                 <!-- Ambulance Image -->
-                <div class="mb-12 mt-16">
+                <div class="mb-8 mt-16 lg:mt-8">
                     <img src="{{ asset('assets/images/ambulance.jpg') }}" alt="Ambulantná fyzioterapia" class="w-full max-w-[65rem] mx-auto rounded-3xl shadow-lg object-cover" style="max-height: 400px;">
                 </div>
 
@@ -111,10 +103,10 @@
                 <!-- Cieľ ambulantnej fyzioterapie -->
                 <div class="text-left space-y-4 relative">
             <!-- Gradient Half Circle - left Goal of -->
-                <div class="lg:flex absolute top-[0.2rem] -left-[2rem] lg:top-0 lg:-left-[8rem] w-40 h-40 pointer-events-none ">
-                    <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-full h-20 lg:h-full object-contain" />
+                <div class="lg:flex absolute top-[0.2rem] -left-[2rem] lg:top-[0.25rem] lg:-left-[4rem] w-40 h-40 pointer-events-none ">
+                    <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-20 h-20 lg:h-20 object-contain" />
                 </div>
-                    <h3 class="text-2xl md:text-3xl font-black mb-6 ml-8 lg:ml-0">Cieľ ambulantnej fyzioterapie</h3>
+                    <h3 class="text-2xl md:text-xl font-black mb-6 ml-8 lg:ml-0">Cieľ ambulantnej fyzioterapie</h3>
 
                     <p class="!mt-[1.5rem] lg:mt-0 ml-8 lg:ml-0">
                         Cieľom ambulantnej fyzioterapie je zlepšiť alebo obnoviť pohybové schopnosti, zmierniť bolesť, podporiť regeneráciu a zvýšiť kvalitu života. Využíva rôzne metódy ako manuálna terapia, cvičenia, masáže.
@@ -126,15 +118,15 @@
                 </div>
                   <div class="max-w-6xl mx-auto relative z-10">
                 <!-- Gradient Half Circle - left -->
-                <div class="lg:flex absolute top-[0.2rem] -left-[2rem] lg:top-0 lg:-left-[8rem] w-40 h-40 pointer-events-none">
-                    <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-full h-20 lg:h-full object-contain" />
+                <div class="lg:flex absolute top-[0.2rem] -left-[2rem] lg:top-[0.25rem] lg:-left-[4rem] w-40 h-40 pointer-events-none">
+                    <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-20 h-20 lg:h-20 object-contain" />
                 </div>
     
 
                 <div class="flex flex-col gap-8 items-start mt-16">
            <div class="flex-1">
-                        <h2 class="text-2xl md:text-4xl lg:text-3xl font-black mb-6 ml-8 lg:ml-0">Typy fyzioterapie</h2>
-                        <p class="text-md md:text-xl text-gray-700 ml-8 lg:ml-0">
+                        <h2 class="text-2xl md:text-xl font-black mb-6 ml-8 lg:ml-0">Typy fyzioterapie</h2>
+                        <p class="text-md md:text-md text-gray-700 ml-8 lg:ml-0">
                             Naše fyzioterapie sú určené predovšetkým starším ľuďom s problémami pohybového aparátu, pacientom po operáciách alebo úrazoch, osobám so zníženou mobilitou, pracujúcim s časovým obmedzením, ľuďom trpiacim chronickými bolesťami & neurologickým pacientom, ktorým chceme uľahčiť a individuálne prispôsobiť fyzioterapiu.
                         </p>
                     </div>
@@ -289,12 +281,12 @@
         <!-- CTA Section -->
         <div class="container mx-auto w-full max-w-6xl lg:max-w-4xl relative z-10 px-[5%] lg:px-0 py-16">
             <!-- Gradient Half Circle CTA - left -->
-            <div class="lg:flex absolute top-[4.3rem] -left-[2rem] lg:top-[3.6rem] lg:-left-[8rem] w-40 h-40 pointer-events-none px-[5%]">
-                <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-full h-20 lg:h-full object-contain" />
+            <div class="lg:flex absolute top-[4.3rem] -left-[2rem] lg:top-[4.25rem] lg:-left-[6.5rem] w-40 h-40 pointer-events-none px-[5%]">
+                <img src="{{ asset('assets/svg/Gradient half circle.svg') }}" alt="" class="w-20 lg:w-20 h-20 lg:h-20 object-contain" />
             </div>
 
-            <h2 class="text-[22px] md:text-3xl lg:text-3xl font-black mb-6 md:mb-8 ml-8 lg:ml-0">Získajte prvú konzultáciu zdarma</h2>
-            <p class="text-md md:text-lg mb-8 md:mb-10 max-w-3xl text-gray-700 ml-8 lg:ml-0">
+            <h2 class="text-[22px] md:text-xl font-black mb-6 md:mb-8 ml-8 lg:ml-0">Získajte prvú konzultáciu zdarma</h2>
+            <p class="text-md md:text-md mb-8 md:mb-10 max-w-3xl text-gray-700 ml-8 lg:ml-0">
                 Sme tu pre Vás, pripravení sprevádzať Vás na ceste k lepšiemu zdraviu, väčšej pohyblivosti a celkovej pohode. Veríme, že každý krok smerom k uzdraveniu má zmysel – a tešíme sa, že môžeme byť jeho súčasťou.
             </p>
             <div class="flex lg:justify-start justify-center">
